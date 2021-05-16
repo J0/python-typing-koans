d927901c0c0b7bd3915e9fb5e408cb102f29f90a
Joel Lee <joel@Joels-MacBook-Pro-3.local>
2021-05-16 15:27:22 +0800

    Add first three solutions

--- koans/py/103-easy-mixed-values.py
+++ koans/py/103-easy-mixed-values.py
@@ -1,8 +1,9 @@
 """
 Koan to learn annotating the Python list of various type of elements
 """
+from typing import Union, List
 # Annotate mixed type-values. There are two-ways to annotate this
-nos: list[int] = [1, 2.0, 3.5]
+nos: list[Union[int, float]] = [1, 2.0, 3.5]
 
 # Annotate the list of int to list of better types
-squares: list[int] = [no * no for no in nos]
+squares: list[Union[int, float]] = [no * no for no in nos]

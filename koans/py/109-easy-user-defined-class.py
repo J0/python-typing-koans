6e1ee006dcc1e01d86e227b4bd3964a0eb78312b
Joel Lee <joel@Joels-MacBook-Pro-3.local>
2021-05-16 16:32:00 +0800

    Update 109

--- koans/py/109-easy-user-defined-class.py
+++ koans/py/109-easy-user-defined-class.py
@@ -4,18 +4,23 @@ Koan to learn annotating the class
 import datetime
 import random
 from string import ascii_letters
-from typing import TypedDict
+from typing import TypedDict, List, ClassVar
 
 
 # Annotate the user data to fit the get_random_user_data
 class UserData(TypedDict):
-    pass
+    name: ClassVar[str]
+    email: ClassVar[str]
+    is_active: ClassVar[bool]
+    last_seen: ClassVar[datetime.datetime]
 
-def get_random_user_data():
-    generated_string: str = "".join(choice(ascii_letters) for x in range(15))
+
+def get_random_user_data() -> UserData:
+    generated_string: str = "".join(
+        random.choice(ascii_letters) for x in range(15))
     return {
         "name": generated_string,
-        "email": f"{name}@gmail.com",
+        "email": f"{generated_string}@gmail.com",
         "is_active": random.choice([True, False]),
         "last_seen": datetime.datetime.utcnow(),
     }
@@ -23,7 +28,7 @@ def get_random_user_data():
 
 class User:
     # Annotate the method arguments
-    def __init__(self, name, email, is_active, last_seen):
+    def __init__(self, name: str, email: str, is_active: bool, last_seen: datetime.datetime):
         self.name = name
         self.email = email
         self.is_active = is_active
@@ -31,14 +36,14 @@ class User:
 
 
 # Annotate input arguments and return type as User
-def create_user(name, email, is_active, last_seen):
+def create_user(name: str, email: str, is_active: bool, last_seen: datetime.datetime) -> User:
     return User(name=name, email=email, is_active=is_active, last_seen=last_seen)
 
 
-def main():
+def main() -> None:
     # Annotate list of users
-    users = []
+    users: List[User] = []
     for _ in range(10):
-        user_data = get_random_user_data()
-        user = create_user(**user_data)
+        user_data: UserData = get_random_user_data()
+        user: User = create_user(**user_data)
         users.append(user)

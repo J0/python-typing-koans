28fbc82f38ae95db2a3016f8ffac3edac9355aed
Joel Lee <joel@Joels-MacBook-Pro-3.local>
2021-05-23 16:30:40 +0800

    fix: add solutions to 110

--- koans/py/110-easy-class-variable.py
+++ koans/py/110-easy-class-variable.py
@@ -4,7 +4,8 @@ Koan to learn annotating the class variables
 import datetime
 import random
 from string import ascii_letters
-from typing import TypedDict
+from typing import TypedDict, ClassVar, Union, Optional, Type
+from random import choice
 
 
 class UserData(TypedDict):
@@ -18,7 +19,7 @@ def get_random_user_data() -> UserData:
     generated_string: str = "".join(choice(ascii_letters) for x in range(15))
     return {
         "name": generated_string,
-        "email": f"{name}@gmail.com",
+        "email": f"{generated_string}@gmail.com",
         "is_active": random.choice([True, False]),
         "last_seen": datetime.datetime.utcnow(),
     }
@@ -27,10 +28,10 @@ def get_random_user_data() -> UserData:
 class User:
     # Annotate the class variable
     # Documentation: https://docs.python.org/3/library/typing.html#typing.ClassVar
-    users: list = []
+    users: ClassVar[list[User]] = []
 
     # Annotate the method arguments
-    def __init__(self, name, email, is_active, last_seen):
+    def __init__(self, name: str, email: str, is_active: bool, last_seen: datetime.datetime):
         self.name = name
         self.email = email
         self.is_active = is_active
@@ -38,27 +39,28 @@ class User:
 
     # Annotate the return type
     @classmethod
-    def add_user(cls, user):
+    def add_user(cls, user: User) -> None:
         cls.users.append(user)
 
     # Annotate the method to return user when found and return None when missing
     @classmethod
-    def get_user(cls, name):
+    def get_user(cls, name: str) -> Optional[User]:
         for user in cls.users:
             if user.name == name:
                 return user
+        return None
 
 
 # Annotate input arguments and return type as User
-def create_user(name, email, is_active, last_seen):
+def create_user(name: str, email: str, is_active: bool, last_seen: datetime.datetime) -> User:
     return User(name=name, email=email, is_active=is_active, last_seen=last_seen)
 
 
-def main():
+def main() -> None:
     for _ in range(10):
         user_data = get_random_user_data()
-        user = create_user(**user_data)
+        user: User = create_user(**user_data)
         User.add_user(user)
 
     # Annotate the variable
-    user = User.get_user(name="Guido")
+    new_user: Optional[User] = User.get_user(name="Guido")

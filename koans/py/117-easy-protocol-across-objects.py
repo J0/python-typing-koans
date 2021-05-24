90e4bab59386ec1106b9718fb0d45a06b9cd5da8
Joel Lee <joel@Joels-MacBook-Pro-3.local>
2021-05-24 22:57:37 +0800

    fix:add solution to 117

--- koans/py/117-easy-protocol-across-objects.py
+++ koans/py/117-easy-protocol-across-objects.py
@@ -2,10 +2,16 @@
 Koan to learn understanding protocol
 """
 import typing as t
+from io import TextIOWrapper
+
+
+class SupportsClose(t.Protocol):
+    def close(self) -> None:
+        ...
 
 
 class FileWrapper:
-    def __init__(self, file, buffer_size: int = 8192) -> None:
+    def __init__(self, file: t.TextIO, buffer_size: int = 8192) -> None:
         self.file = file
         self.buffer_size = buffer_size
 
@@ -15,7 +21,7 @@ class FileWrapper:
 
 
 # implement the protocol which has close method and annotate the files variable
-def close(files) -> None:
+def close(files: t.Iterable[SupportsClose]) -> None:
     for f in files:
         f.close()
 
